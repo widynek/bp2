6c94ae7b41570bd13ee1a0ec437fdd2d9feedc4a
Dawid Widyna <widynek@o2.pl>
2021-08-09 13:45:34 +0200

    create TransactionsRetriever for CSV

--- laravel/app/Services/TransactionsRetrieverCsv.php
+++ laravel/app/Services/TransactionsRetrieverCsv.php
@@ -6,8 +6,73 @@ namespace App\Services;
 
 class TransactionsRetrieverCsv implements TransactionsRetriever
 {
+    private const COLUMN_SEPARATOR = ',';
+
+    /**
+     * @var string
+     */
+    private string $pathToCsvFile;
+
+    public function __construct(string $pathToCsvFile)
+    {
+        $this->pathToCsvFile = $pathToCsvFile;
+    }
+
     public function retrieve(): array
     {
-        return [];
+        $lines = $this->readLines();
+
+        if (count($lines) === 0) {
+            return [];
+        }
+
+        $lineWithColumnNames = \array_shift($lines);
+
+        $columnNames = $this->parseLine($lineWithColumnNames);
+
+        $transactions = [];
+
+        foreach ($lines as $line) {
+            $rowValues = $this->parseLine($line);
+            $transactions[] = \array_combine($columnNames, $rowValues);
+        }
+
+        return $transactions;
+    }
+
+    private function readLines(): array
+    {
+        if (!\file_exists($this->pathToCsvFile)) {
+            throw new \InvalidArgumentException("File {$this->pathToCsvFile}' does not exist");
+        }
+
+        if (!\is_readable($this->pathToCsvFile)) {
+            throw new \InvalidArgumentException("File {$this->pathToCsvFile}' is not readable");
+        }
+
+        $lines = \file($this->pathToCsvFile);
+
+        if ($lines === false) {
+            throw new \RuntimeException("File '{$this->pathToCsvFile}' could not be read");
+        }
+
+        return $lines;
+    }
+
+    private function parseLine(string $line): array
+    {
+        $fields = \str_getcsv($line, self::COLUMN_SEPARATOR);
+
+        return \array_map(function($field) {
+            if (\is_numeric($field) === false) {
+                return $field;
+            }
+
+            if (\strpos($field, '.') !== false) {
+                return (float) $field;
+            }
+
+            return (int) $field;
+        }, $fields);
     }
 }
---
+++ laravel/tests/Unit/Services/TransactionsRetrieverCsvTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Services\TransactionsRetrieverCsv;
+use PHPUnit\Framework\TestCase;
+
+class TransactionsRetrieverCsvTest extends TestCase
+{
+    public function testThrowsExceptionWhenFileWithTransactionDoesNotExist(): void
+    {
+        // arrange
+        $retriever = new TransactionsRetrieverCsv('non-existing.csv');
+
+        // assert
+        $this->expectException(\InvalidArgumentException::class);
+
+        // act
+        $retriever->retrieve();
+    }
+
+    public function testReturnsEmptyArrayForEmptyFile(): void
+    {
+        // arrange
+        $retriever = new TransactionsRetrieverCsv('tests/Artefacts/empty_file');
+
+        // act
+        $content = $retriever->retrieve();
+
+        // assert
+        $this->assertSame([], $content);
+    }
+
+    public function testBuildsAssociativeArrayWithColumnNames(): void
+    {
+        // arrange
+        $retriever = new TransactionsRetrieverCsv('tests/Artefacts/transactions.csv');
+
+        // act
+        $content = $retriever->retrieve();
+
+        // assert
+        $this->assertArrayContainsAllRowsIndexedByColumnNames($content);
+    }
+
+    private function assertArrayContainsAllRowsIndexedByColumnNames(array $content): void
+    {
+        $expectedContent = [
+            [
+                'id' => 1,
+                'code' => 'T_218_ljydmgebx',
+                'amount' => 8617.19,
+                'user_id' => 375,
+                'created_at' => '2020-01-19 16:08:59',
+                'updated_at' => '2020-01-19 16:08:59',
+            ],
+            [
+                'id' => 2,
+                'code' => 'T_335_wmhrbjxld',
+                'amount' => 6502.72,
+                'user_id' => 1847,
+                'created_at' => '2020-01-19 16:08:59',
+                'updated_at' => '2020-01-19 16:08:59',
+            ],
+        ];
+
+        $this->assertSame($expectedContent, $content);
+    }
+
+    public function testMultipleExecutionsProvidesEqualResults(): void
+    {
+        // arrange
+        $retriever = new TransactionsRetrieverCsv('tests/Artefacts/transactions.csv');
+
+        // act
+        $content = $retriever->retrieve();
+
+        // assert
+        $this->assertSame($content, $retriever->retrieve());
+    }
+}
